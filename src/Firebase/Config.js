2b5009edc17c4bd1cb52f90baf1d1be302f0e5fa
Anchusree N <68848197+Anchusree@users.noreply.github.com>
2022-11-21 00:55:59 +0300

    Update Config.js

--- src/Firebase/Config.js
+++ src/Firebase/Config.js
@@ -5,13 +5,13 @@ import 'firebase/storage'
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDdyIfWzSgfRX3Ern6Nkdqxmzt_AnwD2cM",
-  authDomain: "olxproject-3482d.firebaseapp.com",
-  projectId: "olxproject-3482d",
-  storageBucket: "olxproject-3482d.appspot.com",
-  messagingSenderId: "221675229125",
-  appId: "1:221675229125:web:3540b1bbc9fcbe484d05ac",
-  measurementId: "G-Y5LGERBZ96"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
   export default firebase.initializeApp(firebaseConfig)
